76d22cafb2db03f4de075169a2da3451d32203f1
Steven He <hez2010@outlook.com>
2019-12-25 22:52:55 +0800

    Fix bugs

--- MultipleCycleCPU.srcs/sources_1/new/IR.v
+++ MultipleCycleCPU.srcs/sources_1/new/IR.v
@@ -26,7 +26,7 @@ module IR(
     input [31:0] DataIn,
     output reg [31:0] DataOut
     );
-    always @(negedge CLK) begin
+    always @(posedge CLK) begin
         if (IRWre) begin
             DataOut <= DataIn;
         end
